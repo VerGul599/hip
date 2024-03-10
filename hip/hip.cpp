5705284d36e812be3661458aa304ac7737b881af
Вероника <@WIN-BBNSI5FE1UN>
2024-03-10 21:16:20 +0500

    Добавьте файлы проекта.

---
+++ hip/hip.cpp
@@ -0,0 +1,22 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a;
+    std::cout << "Введите значение a: ";
+    std::cin >> a;
+
+    int sum = 0;
+    for (int i = a; i <= 500; i++) 
+    {
+        sum += i;
+    }
+
+    std::cout << "Сумма целых чисел от " << a << " до 500 равна: " << sum;
+
+    return 0;
+}
